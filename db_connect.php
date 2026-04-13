b52ede33a70a63ba00df72e05384409fc9c61c95
Jasmine Hill <jhill39@art.edu>
2026-04-13 10:19:02 -0500

    db-connect.php update

--- db_connect.php
+++ db_connect.php
@@ -1,13 +1,16 @@
 <?php
 
-$host = "127.0.0.1";
+$host = "localhost";
 $dbname = "u443959289_products";
 $username = "u443959289";
-$password = "EleanorJ2023!!";
+$password = "EleanorJ2023!";
 
 $conn = new mysqli($host, $username, $password, $dbname);
 
-if($conn->connect_error) {
-	die("Connection failed: " . $conn->connect_error);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
 }
+
+$conn->set_charset("utf8mb4");
+
 ?>
